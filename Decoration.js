9b7b1409eb9a318e8cfa012523a6ff28e285f9cf
polipolikont <poli@polikont.bg>
2016-09-13 13:41:45 +0300

    Първо потвърждение
    дддд

---
+++ Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("pk.theme.classic.Decoration",
+{
+  extend : qx.theme.classic.Decoration,
+
+  decorations :
+  {
+  }
+});
---
+++ Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("pk.theme.classic.Theme",
+{
+  meta :
+  {
+    color : pk.theme.classic.Color,
+    decoration : pk.theme.classic.Decoration,
+    font : pk.theme.classic.Font,
+    appearance : pk.theme.classic.Appearance,
+    icon : qx.theme.icon.Oxygen
+  }
+});
